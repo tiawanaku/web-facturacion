89c9ef2fbe667c69b314e34d4278487cb4151385
alanezfranz <alanezfranz@gmail.com>
2024-01-22 21:41:06 -0400

    hasta aqui funciona

--- php-includes/getDescripcionProducto.php
+++ php-includes/getDescripcionProducto.php
@@ -1,36 +1,41 @@
 <?php
-// Incluye el archivo de conexión
-include('connect.php');
+// Este es un comentario de una sola línea que inicia el bloque de comentarios
 
-$descripcionProducto = "";
+// Incluye el archivo de conexión (Comentado para desactivar)
+// include('connect.php');
+
+//$descripcionProducto = "";
 
 // Verifica si el código del producto está establecido
-if(isset($_GET['codigoProducto'])) {
-    $codigoProducto = $_GET['codigoProducto'];
+//if(isset($_GET['codigoProducto'])) {
+    //$codigoProducto = $_GET['codigoProducto'];
 
     // Prepara la consulta SQL
-    $query = mysqli_prepare($con, "SELECT descripcionProducto FROM cegepafacturacion.sincronizarlistaproductosservicios WHERE codigoProducto = ? LIMIT 1");
-    mysqli_stmt_bind_param($query, "s", $codigoProducto);
+    // $query = mysqli_prepare($con, "SELECT descripcionProducto FROM cegepafacturacion.sincronizarlistaproductosservicios WHERE codigoProducto = ? LIMIT 1");
+    // mysqli_stmt_bind_param($query, "s", $codigoProducto);
 
     // Ejecuta la consulta
-    mysqli_stmt_execute($query);
+    // mysqli_stmt_execute($query);
 
     // Almacena el resultado
-    $resultado = mysqli_stmt_get_result($query);
+    // $resultado = mysqli_stmt_get_result($query);
 
     // Obtiene la fila del resultado
-    $fila = mysqli_fetch_assoc($resultado);
+    // $fila = mysqli_fetch_assoc($resultado);
 
     // Guarda la descripción del producto
-    if ($fila) {
-        $descripcionProducto = $fila['descripcionProducto'];
-    }
+    // if ($fila) {
+    //     $descripcionProducto = $fila['descripcionProducto'];
+    // }
 
     // Cierra la consulta
-    mysqli_stmt_close($query);
-}
+    // mysqli_stmt_close($query);
+//}
 
 // Cierra la conexión a la base de datos
-mysqli_close($con);
+// mysqli_close($con);
+
+// Fin del bloque de comentarios
 ?>
+
 
